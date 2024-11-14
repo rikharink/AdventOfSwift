7be636c776e452c0748935be44f01848fab9f68c
Rik Harink <rik@har.ink>
2024-11-14 19:48:23 +0100

    Initial Commit

---
+++ AdventOfSwift/AdventOfSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfSwiftApp.swift
+//  AdventOfSwift
+//
+//  Created by Rik Harink on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventOfSwiftTests/AdventOfSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfSwiftTests.swift
+//  AdventOfSwiftTests
+//
+//  Created by Rik Harink on 14/11/2024.
+//
+
+import Testing
+@testable import AdventOfSwift
+
+struct AdventOfSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
